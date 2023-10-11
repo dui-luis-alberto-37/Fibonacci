775c5dc7608fbb69d4a08665c96f4503ecd5a448
dui-luis-alberto <lgarciaorozco6@gmail.com>
2023-10-11 12:16:04 -0600

    secuence and fibo

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Fibonacci series
+# By: lgarciaorozco6@gmail.com
+# 11/10/23
+# LICENSE: GNU/GPL
+
+# n = int(input())
+n = 5
+fibo = [0,1]
+if n > 2:
+    for _ in range(n-2):
+        fibo
---
+++ secuence.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# My first secuence
+# By: lgarciaorozco6@gmail.com
+# 11/10/23
+# LICENSE: GNU/GPL
+
+suma = 0
+
+for i in range(11):
+    suma += i
+
+print(suma)
