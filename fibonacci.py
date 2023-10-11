fe697cdebfb82a08d87a6f722571cd0e437efda3
dui-luis-alberto <lgarciaorozco6@gmail.com>
2023-10-11 13:01:26 -0600

    golden num

--- fibonacci.py
+++ fibonacci.py
@@ -3,9 +3,11 @@
 # By: lgarciaorozco6@gmail.com
 # 11/10/23
 # LICENSE: GNU/GPL
+from math import sqrt
 
 n = int(input())
 fibo = [1,1]
+
 if n > 1:
     for _ in range(n-2):
         fibo.append(fibo[-2]+fibo[-1])
@@ -17,3 +19,6 @@ else: print(fibo)
 # for i in range(len(fibo)-1):
     # print(fibo[i+1])
 
+razones = [fibo[i+1]/fibo[i] for i in range(len(fibo)-1)]
+print(sum(razones)/len(razones),(1+sqrt(5))/2)
+    
