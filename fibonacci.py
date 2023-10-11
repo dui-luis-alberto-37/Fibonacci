80ff2ca31f9a87e4883627c0d8a187a8a33bdbd6
dui-luis-alberto <lgarciaorozco6@gmail.com>
2023-10-11 12:33:18 -0600

    fibo

--- fibonacci.py
+++ fibonacci.py
@@ -4,9 +4,15 @@
 # 11/10/23
 # LICENSE: GNU/GPL
 
-# n = int(input())
-n = 5
+n = int(input())
 fibo = [0,1]
-if n > 2:
-    for _ in range(n-2):
-        fibo
+if n > 1:
+    for _ in range(n-1):
+        fibo.append(fibo[-2]+fibo[-1])
+    # print(fibo[-1])
+# else:
+    # print(fibo[n])
+
+for i in range(len(fibo)-1):
+    print(fibo[i+1])
+
