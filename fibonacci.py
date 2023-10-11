001b59749d6df8a439d7a572df2b85ab021258a0
dui-luis-alberto <lgarciaorozco6@gmail.com>
2023-10-11 12:42:34 -0600

    vector

--- fibonacci.py
+++ fibonacci.py
@@ -5,14 +5,15 @@
 # LICENSE: GNU/GPL
 
 n = int(input())
-fibo = [0,1]
+fibo = [1,1]
 if n > 1:
-    for _ in range(n-1):
+    for _ in range(n-2):
         fibo.append(fibo[-2]+fibo[-1])
-    # print(fibo[-1])
-# else:
-    # print(fibo[n])
+    print(fibo)
+elif n == 1:
+    print(fibo[:1])
+else: print(fibo)
 
-for i in range(len(fibo)-1):
-    print(fibo[i+1])
+# for i in range(len(fibo)-1):
+    # print(fibo[i+1])
 
